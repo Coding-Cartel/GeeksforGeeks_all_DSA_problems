15429ee58575be022aef30848d6d3bbf1f2e6aa3
tusharbaskheti18 <baskhetitushar18@gmail.com>
2023-10-20 11:28:43 +0530

    added new gfg DSA problem

---
+++ Odd_or_Even.cpp
@@ -0,0 +1,11 @@
+class Solution{   
+public:
+    string oddEven(int N){
+        // code here 
+        if(N%2==0)
+        {
+            return "even";
+        }
+        return "odd";
+    }
+};
