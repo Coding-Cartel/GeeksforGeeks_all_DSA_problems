1821d1b875940af2c3bfdb80599d4b89acad7552
tusharbaskheti18 <baskhetitushar18@gmail.com>
2023-10-21 18:09:57 +0530

    added new gfg DSA problem

---
+++ Java_Inheritance.cpp
@@ -0,0 +1,15 @@
+class cls2 extends cls1
+{
+    void mul(int p,int q)
+    {
+        System.out.println(p*q);
+    }
+    void task(int p,int q)
+    {
+        //Add your code here.
+        int aSquare=p*p;
+        int bSquare=q*q;
+        int sum = aSquare+bSquare;
+        System.out.println(sum);
+    }
+}
